00dd84610c7c881000a0460ce73b03962525b99e
Shradha Holay <shraddhaholay@gmail.com>
2022-07-18 17:02:44 +1200

    navbar css updated

--- shrisapp/src/components/Navbar.js
+++ shrisapp/src/components/Navbar.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+// import {sidebar} from './components/SidebarData';
+import { SidebarData } from './SidebarData';
+import './Navbar.css';
 
 function Navbar() {
+  const {sidebar, setSidebar} = useState(false);
+  const showSidebar =() => setSidebar(!sidebar);
+
   return (
     <>
     <div className="Navbar">
@@ -10,6 +17,28 @@ function Navbar() {
       <FaIcons.FaBars/>
       </Link>
     </div>
+    <nav className={sidebar ? 'nav-menu active' : 'nav-menu' }>
+      <ul className='nav-menu-items'>
+        <li className={'navbar-toggle'}> 
+        <Link to="#" className='menu-bars'>
+         <AiIcons.AiOutlineClose/>
+         </Link>
+        </li>
+        {SidebarData.map((item,index)=>{
+            return(
+              <li key={index} className={item.cName}>
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+
+                </Link>
+
+              </li>
+            )
+
+        })}
+      </ul>
+    </nav>
     </>
   );
 }
