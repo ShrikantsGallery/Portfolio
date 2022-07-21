76f558272c73368fcb04cf866ff65ac4bc117649
Shradha Holay <shraddhaholay@gmail.com>
2022-07-21 19:57:45 +1200

    css design added in brown choc colour

--- shrisapp/src/pages/Home.js
+++ shrisapp/src/pages/Home.js
@@ -4,11 +4,18 @@ import './Home.css';
 function Home() {
   return (
     <div className='home'>
-      <div class='at-container'>
-        <h1 class='at-item'>
-          ShriSnaps
-        </h1>
-      </div>
+      <div className='home-grid'>
+        <div id="item-0">
+          <div class='at-container'>
+            <h1 class='at-item'>
+              ShriSnaps
+            </h1>
+          </div>
+        </div>
+        <div id="item-1">filter</div>
+        <div id="item-2">Photos </div>
+        <div id="item-3">Stories</div>
+        </div>
     </div>
   );
 }
